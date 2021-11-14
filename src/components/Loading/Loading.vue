bb393068ec2f4a579c5d30956f93a4b25c62b6a9
victorhugo.carreira <victor.carreira@itriad.org.br>
2021-11-14 20:30:43 -0300

    Adicionando projeto e componente loading

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-content">
+    <img src="../../assets/loading-burger.png" width="200" />
+    <div class="loading">
+      <h3>Loading</h3>
+      <div class="loading-bar">
+        <div class="percentage" :style="{ width: percentage + '%' }"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./Loading.css";
+// import router from "../../router";
+
+export default {
+  name: "Loading",
+  
+  methods: {
+  // redirectPage() {
+  //   setInterval(() => {
+  //     // alert("Mounted Loading");
+  //     // router.push("/about") ;
+  //   }, 3000);
+  // },  
+  },
+
+  mounted() {
+    console.log("Componente Loading inicializado")
+  },
+  
+};
+</script>
